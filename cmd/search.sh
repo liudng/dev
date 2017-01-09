22b5cb6ea200a2ee8885cd452f3ab5a746f0ef97
liudng <liudng@gmail.com>
2017-01-09 13:15:30 +0000

    Add new search command.

--- cmd/search.sh
+++ cmd/search.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 glb_sta=1
+glb_run_compact=1
 
 cmd_main() {
-    
+    grep -r --color \
+        --exclude=*~ \
+        --exclude=*.log \
+        --exclude=*.swp \
+        --exclude=tags \
+        --exclude-dir=.git \
+        "$1" .
 }
