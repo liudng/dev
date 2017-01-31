4ab5c8bb0d839723664709bc623276240c3acd01
liudng <liudng@gmail.com>
2017-01-31 21:57:50 +0800

    copyright

--- bin/dev.bash
+++ bin/dev.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright 2017 The dev Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 
 #
 # Usage:
