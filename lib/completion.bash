c95fc4039642ecb7d963341813392c00782f633b
liudng <liudng@gmail.com>
2017-09-27 18:27:55 +0800

    Reflect completion.bash

--- lib/completion.bash
+++ lib/completion.bash
@@ -2,46 +2,35 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-
-# COMPREPLY: array holds the completion results that gets displayed after
-#             pressing [TAB][TAB]
-#
-# COMP_WORDS: array of words that is typed on the command line
-#
-# COMP_CWORD: index for the COMP_WORDS array and using this different position
-#             words on command line can be accessed.
-#
-# compgen : -W holds the possible completions and the respective argument get
-#           chosen based on the $current_arg.
-#
-# By convention, the function name starts with an underscore.
-# .bash_completion
-# source ./lib/completion.bash
-
-
 #
 # dev completion
+# By convention, the function name starts with an underscore.
 #
 _dev_init_completion() {
     # Pointer to current completion word.
     # By convention, it's named "cur" but this isn't strictly necessary.
-    local arr cur prev opts
+    local arr cur opts
 
+    # COMPREPLY: array holds the completion results that gets displayed after
+    #             pressing [TAB][TAB]
     COMPREPLY=() # Array variable storing the possible completions.
+
+    # COMP_WORDS: array of words that is typed on the command line
+    #
+    # COMP_CWORD: index for the COMP_WORDS array and using this different position
+    #             words on command line can be accessed.
     cur=${COMP_WORDS[COMP_CWORD]}
-    prev=${COMP_WORDS[COMP_CWORD-1]}
 
     if [ "${cur:0:1}" == "-" ]; then
         opts=$(dev ":options" 2>/dev/null) #
     else
-        arr=(${COMP_WORDS[@]})
-        unset arr[COMP_CWORD]
+        arr=(${COMP_WORDS[@]}) && unset arr[COMP_CWORD]
         [ "${arr[0]}" == "dev" ] && unset arr[0]
-        # echo "cur:$cur" >&2
-        # echo "arr:${arr[@]:1}" >&2
-        opts=$(dev ":completion" ":$cur" "${arr[@]}" 2>/dev/null) #
+        opts=$(dev ":completion" ":$cur" "${arr[@]}" 2>/dev/null)
     fi
 
+    # compgen : -W holds the possible completions and the respective argument get
+    #           chosen based on the $current_arg.
     COMPREPLY=( $(compgen -W "$opts" -- "$cur" ) );
 }
 
